9e899776205e0dd0b371ba03cf8364928d0f2029
Ratan Kaliani <ratankaliani@berkeley.edu>
2021-12-29 01:15:21 -0500

    In progress of adding sort method to sort on endTime, running into issues with getting the end_time from proposal

--- server/scripts/proposalList.py
+++ server/scripts/proposalList.py
@@ -5,11 +5,25 @@ from server.scripts.uniswap import Uniswap
 
 import time
 from server.models.proposal import Proposal  # noqa: E501
+from heapq import merge
+from operator import itemgetter
+
+
+# function for meging k arrays
+def mergeK(arr, k):
+     
+    l = arr[0]
+    for i in range(k-1):
+        l = list(merge(l, arr[i + 1], key = itemgetter("end_time")))
+    return l
+
 
 # Assumes timeblock > 0
 # if platforms is empty, query all
 # else check if platforms (lowercase) are supported
 def getProposals(timeblock, queryPlatforms):
+
+    queryAll = len(queryPlatforms) == 0
 
     supportedPlatforms = set(
        [
@@ -20,22 +34,30 @@ def getProposals(timeblock, queryPlatforms):
     )
 
     platforms = supportedPlatforms.intersection(queryPlatforms)
-
+    
     cutoff = time.time() - (timeblock)
     # get all proposals
-    queryAll = len(queryPlatforms) == 0
+    
+    if queryAll:
+        numPlatforms = len(supportedPlatforms)
+    else:
+        numPlatforms = len(platforms)
     
     allProposals = []
+
+    # Sort K sorted arrays on endTime
     if queryAll or 'aave' in platforms: 
         aaveProposals = Aave.getProposalsFast(timeblock)
-        allProposals.extend(aaveProposals)
+        allProposals.append(aaveProposals)
     if queryAll or 'compound' in platforms:
         compProposals = Compound.getProposalsFast(timeblock)
-        allProposals.extend(compProposals)
+        allProposals.append(compProposals)
     if queryAll or 'uniswap' in platforms:
         uniswapProposals = Uniswap.getProposalsFast(timeblock)
-        allProposals.extend(uniswapProposals)
+        allProposals.append(uniswapProposals)
     
+    allProposals = mergeK(allProposals, numPlatforms)
+
     
     print("AllProposals length: " + str(len(allProposals)))
 
@@ -45,4 +67,5 @@ def getProposals(timeblock, queryPlatforms):
         if proposal.end_time and proposal.end_time > cutoff:
             recentProposals.append(proposal)
     
+
     return recentProposals
